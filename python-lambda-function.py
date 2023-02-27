abf99a2362b3a0b3b46b65c71fc8342f51cfc36e
dmitriybursak <dmitriy.bursak.work@gmail.com>
2023-02-27 16:36:32 +0200

    Added DrankLiquidIntent handler

--- python-lambda-function.py
+++ python-lambda-function.py
@@ -13,45 +13,145 @@ except Exception as e:
 
 openai.api_key = openAIAPiKey
 
-def lambda_handler(event, context):
-    # Passing the message to OpenAI
-    query = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=("The following is a casual conversation between two friends.\n\nFriend: " + event['inputTranscript'] + "\nAI: "),
-        temperature=0.5,
-        max_tokens=100,
-        presence_penalty=0.6,
-        stop=["Friend: ","AI: "]
-    )
-    content = query.choices[0].text
-    
-    # Printing the details for CloudWatch Logs
-    print({
-        "in": {
-            "transcript": event['inputTranscript'],
-            "confidence": event['transcriptions'][0]['transcriptionConfidence']
-        },
-        "out": {
-            "message": content,
-            "gptTokens": query.usage.total_tokens
-        }
-    })
-    
-    # Returning the object to be passed back to Lex
+
+def get_session_attributes(intent_request):
+    sessionState = intent_request['sessionState']
+    if 'sessionAttributes' in sessionState:
+        return sessionState['sessionAttributes']
+
+    return {}
+
+
+def get_slots(intent_request):
+    return intent_request['sessionState']['intent']['slots']
+
+
+def get_slot(intent_request, slotName):
+    slots = get_slots(intent_request)
+    if slots is not None and slotName in slots and slots[slotName] is not None:
+        return slots[slotName]['value']['interpretedValue']
+    else:
+        return None
+
+
+def close(intent_request, session_attributes, fulfillment_state, message):
+    intent_request['sessionState']['intent']['state'] = fulfillment_state
     return {
-        "sessionState": {
-            "dialogAction": {
-                "type": "ElicitIntent"
+        'sessionState': {
+            'sessionAttributes': session_attributes,
+            'dialogAction': {
+                'type': 'Close'
             },
-            "intent": {
-                "name": "ChatGPT_Intent",
-                "state": "Fulfilled"
-            }
+            'intent': intent_request['sessionState']['intent']
         },
-        "messages": [
-            {
-                "contentType": "PlainText",
-                "content": content
-            }
-        ]
+        'messages': [message],
+        'sessionId': intent_request['sessionId'],
+        'requestAttributes': intent_request['requestAttributes'] if 'requestAttributes' in intent_request else None
     }
+
+
+def elicit_intent(intent_request, session_attributes, message):
+    return {
+        'sessionState': {
+            'dialogAction': {
+                'type': 'ElicitIntent'
+            },
+            'sessionAttributes': session_attributes
+        },
+        'messages': [message] if message != None else None,
+        'requestAttributes': intent_request['requestAttributes'] if 'requestAttributes' in intent_request else None
+    }
+
+
+def lambda_handler(event, context):
+    intent_name = event['sessionState']['intent']['name']
+    if intent_name == 'DrankLiquidIntent':
+        session_attributes = get_session_attributes(event)
+        numberOfGlass = get_slot(event, 'numberOfGlass')
+        sizeOfBeverage = get_slot(event, 'sizeOfBeverage')
+
+        print("session_attributes")
+        print(session_attributes)
+
+        if "small" in session_attributes and "medium" in session_attributes and "large":
+            # add some water to previous result
+            print("add some water to previous result")
+            session_attributes[sizeOfBeverage] = int(session_attributes[sizeOfBeverage]) + int(numberOfGlass)
+        else:
+            # add some water first time
+            print("add some water first time")
+            updated_session_attributes = {
+                "small": 0,
+                "medium": 0,
+                "large": 0
+            }
+            updated_session_attributes[sizeOfBeverage] = numberOfGlass
+            session_attributes = updated_session_attributes
+
+        print(session_attributes)
+
+        flag = False
+        text = "Ok. You drunk "
+        if int(session_attributes['small']) > 1:
+            text = text + str(session_attributes['small']) + " small glasses"
+            flag = True
+        if int(session_attributes['medium']) > 1:
+            if flag:
+                text = text + " and"
+            text = text + " " + str(session_attributes['medium']) + " medium glasses"
+            flag = True
+        if int(session_attributes['large']) > 1:
+            if flag:
+                text = text + " and"
+            text = text + " " + str(session_attributes['large']) + " large glasses"
+        text = text + " of water today."
+
+        message = {
+            'contentType': 'PlainText',
+            'content': text
+        }
+        fulfillment_state = "Fulfilled"
+        return close(event, session_attributes, fulfillment_state, message)
+    else:
+        # Passing the message to OpenAI
+        query = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=("The following is a casual conversation between two friends.\n\nFriend: " + event[
+                'inputTranscript'] + "\nAI: "),
+            temperature=0.5,
+            max_tokens=100,
+            presence_penalty=0.6,
+            stop=["Friend: ", "AI: "]
+        )
+        content = query.choices[0].text
+
+        # Printing the details for CloudWatch Logs
+        print({
+            "in": {
+                "transcript": event['inputTranscript'],
+                "confidence": event['transcriptions'][0]['transcriptionConfidence']
+            },
+            "out": {
+                "message": content,
+                "gptTokens": query.usage.total_tokens
+            }
+        })
+
+        # Returning the object to be passed back to Lex
+        return {
+            "sessionState": {
+                "dialogAction": {
+                    "type": "ElicitIntent"
+                },
+                "intent": {
+                    "name": "FallbackIntent",
+                    "state": "Fulfilled"
+                }
+            },
+            "messages": [
+                {
+                    "contentType": "PlainText",
+                    "content": content
+                }
+            ]
+        }
